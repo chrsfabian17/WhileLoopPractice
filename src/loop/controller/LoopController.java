77d22b1deaacc9b377fe0d349cd2fb167e1f3332
chrsfabian17 <chrsfabian17@gmail.com>
2016-09-24 02:01:30 -0600

    Created Multiple loops

---
+++ src/loop/controller/LoopController.java
@@ -0,0 +1,47 @@
+package loop.controller;
+
+public class LoopController
+{
+public void start()
+    {
+	whileLoopTest();
+	forLoopTest();
+    }
+
+     private void whileLoopTest()
+    {
+	int count = 0;
+	while (count < 12)
+	{
+		System.out.println("Count is: " + count);
+		
+		count++;
+	}
+	    System.out.println("Its like 1:00AM, but I love code!:))");
+	{
+	int loopCount = 0;
+	while (loopCount < 4 )
+	{
+		System.out.println("I believe this is the " + loopCount + "th time though the loop");
+		
+		loopCount++;
+	}
+	
+//	fix
+	private void forLoopTest()
+	{	
+		System.out.println("Im KIDDING, you're not Geeks, just FREAKS! ;)");
+	for(int b = 0; b < 6; b++)
+		System.out.println("SEE THAT RHYME?!?! I could be a rhyme machine!!!!!!!!!!!!!!!!");
+		{
+	for(int c = 1; c < 3; c++)
+		System.out.println("Im going to do this until I think of a ryhme.");
+		}
+		System.out.println("I got it, im like a roket! ;))");
+	
+	}
+}
+	
+		
+
+
---
+++ src/loop/controller/LoopRunner.java
@@ -0,0 +1,13 @@
+package loop.controller;
+
+public class LoopRunner 
+
+{
+		public static void main (String [] args)
+		{
+			LoopController loopAppController = new LoopController();
+			loopAppController.start();
+			
+		}
+
+}
